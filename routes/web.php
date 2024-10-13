547a8fd5e21d0061e06eb37c542dc38a37ea5e5b
ardhikakrsh <ardhikakrishna@gmail.com>
2024-10-13 20:26:58 +0700

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\admin\MahasiswaController;
+use App\Http\Controllers\admin\BarangController;
+use App\Http\Controllers\admin\PembelianController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/dashboard', [DashboardController::class, 'admin']);
+Route::resource('barang', BarangController::class);
+Route::resouce('pembelian', PembelianController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
-
