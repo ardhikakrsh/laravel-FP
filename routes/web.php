680c3580c9a82e0d41e04454e79ded6754afcd33
ardhikakrsh <ardhikakrishna@gmail.com>
2024-10-14 16:42:06 +0700

    feat: add users route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\MahasiswaController;
 use App\Http\Controllers\admin\BarangController;
+use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\admin\PembelianController;
 use App\Http\Controllers\DashboardController;
 
@@ -12,6 +13,6 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardController::class, 'admin']);
-Route::resource('barang', BarangController::class);
-Route::resouce('pembelian', PembelianController::class);
-Route::resource('mahasiswa', MahasiswaController::class);
+Route::resource('/barang', BarangController::class);
+Route::resource('/users', UserController::class);
+Route::resource('/pembelian', PembelianController::class);
