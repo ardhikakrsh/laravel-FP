8d8ecd7978b1be89bbb6d189145eab581d3c5eef
ardhikakrsh <ardhikakrishna@gmail.com>
2024-10-17 13:58:33 +0700

    feat: add auth route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\admin\MahasiswaController;
 use App\Http\Controllers\admin\BarangController;
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\admin\PembelianController;
 use App\Http\Controllers\DashboardController;
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'admin']);
-Route::resource('/barang', BarangController::class);
-Route::resource('/users', UserController::class);
-Route::resource('/pembelian', PembelianController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'admin'])->name('dashboard');
+    Route::get('/about', [DashboardController::class, 'admin'])->name('about');
+    Route::get('/product', [DashboardController::class, 'admin'])->name('product');
+    Route::get('/contact', [DashboardController::class, 'admin'])->name('contact');
+    Route::get('/review', [DashboardController::class, 'admin'])->name('review');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/barang', BarangController::class);
+    Route::resource('/users', UserController::class);
+    Route::resource('/pembelian', PembelianController::class);
+});
+
+require __DIR__.'/auth.php';
+
+
--- storage/routes/web.php
+++ storage/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\MahasiswaController;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::resource('mahasiswa', MahasiswaController::class);
+// Route::resource('mahasiswa', MahasiswaController::class);
 
